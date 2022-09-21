5fa1e11fe164a84e97a6ac0b7fb74173ab8f86d3
rnainwal <rnainwal@prometheusgroup.com>
2022-09-21 22:10:51 +0530

    Typescript support added.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
-
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Initial from "./src/navigations/Initial";
@@ -9,6 +9,7 @@ import Initial from "./src/navigations/Initial";
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
       <NavigationContainer>
         <Initial />
       </NavigationContainer>
